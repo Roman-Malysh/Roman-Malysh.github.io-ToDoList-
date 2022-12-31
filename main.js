63a0046812126ae39f157d96e8aeeabab00184d5
Roman <romanmalysh@romans-macbook-air.home>
2022-12-31 03:57:09 +0100

    Somefeatures

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict"
 
+//variables
+
 const form = document.querySelector('.form');
 const input = document.querySelector('.input__input');
 const inputWrap = document.querySelector('.input');
@@ -9,6 +11,9 @@ const deleteTask = document.querySelector('.task__delete');
 const noTask = document.querySelector('.noTaskLeft');
 const deleteAll = document.querySelector('.deleteAll');
 const allTasks = document.querySelectorAll('.task');
+const label = document.getElementById('.label');
+
+//delete button
 
 deleteAll.addEventListener('click', () => {
   const allTasks = document.querySelectorAll('.task');
@@ -18,24 +23,23 @@ deleteAll.addEventListener('click', () => {
   }
 });
 
-function noTaskLeft() {
-  buttonChecker();
-  if (form.children.length > 3) {
-    noTask.innerHTML = 'Tasks to do';
-  } else {
-    noTask.innerHTML = 'No Tasks Left';
-  }
-}
+//all click task checker
 
-function buttonChecker() {
-  const tasks = document.querySelectorAll('.task');
+document.querySelector('.page').addEventListener('click', () => {
+    let tasksCounter = form.children.length - 3;
+    console.log(form.children.length );
 
-  tasks.forEach(el => console.log(el));
-}
-
-document.querySelector('.page').addEventListener('click', (e) => {
-  noTaskLeft();
+    function taskChecker() {
+      if (form.children.length > 3) {
+        noTask.innerHTML = `You have ${tasksCounter} tasks to do`;
+      } else {
+        noTask.innerHTML = 'No Tasks Left';
+      }
+    }
+    taskChecker();
 });
+
+//add task
 
 inputWrap.addEventListener('click', (e) => {
   e.preventDefault();
@@ -47,8 +51,11 @@ inputWrap.addEventListener('click', (e) => {
   if (e.target.matches('.input__button')) {
     form.insertAdjacentHTML('beforeend', `
     <div class="task">
-    <a class="task__toggler task__toggler--done"></a>
-    <input 
+    <div class="round">
+      <input type="checkbox" checked id="checkbox">
+      <label for="checkbox" id="label"></label>
+  </div>
+    <input
         type="text"
         class="task__input"
         value="${input.value}"
@@ -68,14 +75,12 @@ inputWrap.addEventListener('click', (e) => {
   }
 });
 
+//form events
+
 form.addEventListener('click', (e) => {
   e.preventDefault();
   
-  let value = e.target.closest('.task').children[1].value;
-
-  const target = e.target.closest('.task').children[1];
-
-
+  console.log(e.target);
   switch (true) {
     case (e.target.matches('.task__delete')) :
       e.target.closest('.task').remove();
@@ -83,6 +88,15 @@ form.addEventListener('click', (e) => {
       return;
     
     case (e.target.matches('.task__edit')) :
+      const allTheTasks = document.querySelectorAll('.task'); 
+    for (const task of allTheTasks){
+      task.children[1].setAttribute('readonly', true);
+      task.children[2].classList.add('task__edit');
+      task.children[2].classList.remove('task__save');
+      task.children[2].innerHTML = 'Edit';
+    } 
+      const target = e.target.closest('.task').children[1];
+      let value = e.target.closest('.task').children[1].value;
       target.removeAttribute('readonly');
       target.focus();
       target.value = '';
@@ -94,7 +108,7 @@ form.addEventListener('click', (e) => {
       return;
 
     case (e.target.matches('.task__save')) :
-      target.setAttribute('readonly', true);
+      e.target.closest('.task').children[1].setAttribute('readonly', true);
       e.target.closest('.task').children[2].classList.add('task__edit');
       e.target.closest('.task').children[2].classList.remove('task__save');
       e.target.closest('.task').children[2].innerHTML = 'Edit';
@@ -105,29 +119,17 @@ form.addEventListener('click', (e) => {
       e.target.closest('.task').children[2].classList.remove('task__toggler');
       e.target.closest('.task').children[2].classList.add('task__toggler--done');
 
+      return;
+
+    case (e.target.matches('#label')) :
+      e.target.closest('.round').closest('.task').children[0].style.backgroundColor = 'rgba(136,202,108, 0.2)';
+      e.target.closest('.round').closest('.task').children[1].style.backgroundColor = 'rgba(136,202,108, 0.2)';
+      e.target.closest('.round').closest('.task').children[2].style.backgroundColor = 'rgba(136,202,108, 0.2)';
+      e.target.closest('.round').closest('.task').children[3].style.backgroundColor = 'rgba(136,202,108, 0.7)';
+      e.target.closest('.round').closest('.task').style.backgroundColor = 'rgba(136,202,108, 0.7)';
+      e.target.closest('.round').closest('.task').children[1].style.textDecoration = 'line-through';
+  
+    return;
   }
 });
 
-form.addEventListener('click', (e) => {
-
-
-  const allTasks = document.querySelector('.task');
-
-  if (allTasks.children.length) {
-    console.log(allTasks.children.length);
-  }
-
-// Convert buttons NodeList to an array
-  var btnsArr = Array.prototype.slice.call(allTasks);
-
-  console.log(btnsArr);
-
-  for (const item of allTasks) {
-    if (item.children[2].className === 'task__save' && !e.target.item.children.className === 'task__save') {
-      for (const key of allTasks) {
-        key.children[2].classList.remove('task__save');
-        key.children[2].classList.add('task__edit');
-      }
-    }
-  }
-});
